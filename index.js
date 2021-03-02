dcc0ee90213408d509ba796b2c12445a4b818b08
Ben <brbryant2639@gmail.com>
2021-03-01 19:01:49 -0600

    Set up exercise for class

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const dataPath = path.join(__dirname, "./index.html");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.use(express.json());
+
+app.get("/pokemon", (req, res) => {
   try {
     res.status(200).sendFile(dataPath);
   } catch (error) {
@@ -13,5 +16,48 @@ app.get("/", (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server listening on port 3000..."));
+app.put("/pokemon/:id", (req, res, next) => {
+  try {
+    let { id } = req.params;
+    let { body } = req.body;
 
+    fs.readFile(dataPath, (err, data) => {
+      if (err) {
+        next(err);
+      }
+      data.map((pokemon) => {
+        if (pokemon.id === id) {
+          console.log("FOUND!");
+        }
+      });
+    });
+
+    res.status(200).json({ msg: "Pokemon Updated!" });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred on the server" });
+  }
+});
+
+app.post("/pokemon", (req, res) => {
+  try {
+    let { body } = req;
+    res.status(200).json({ msg: "Pokemon Added!" });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred on the server" });
+  }
+});
+
+app.get("*", (req, res) => {
+  try {
+    res.status(404).send("404 Not Found");
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred on the server" });
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ errors: { err: err.message } });
+});
+
+app.listen(3000, () => console.log("Server listening on port 3000..."));
